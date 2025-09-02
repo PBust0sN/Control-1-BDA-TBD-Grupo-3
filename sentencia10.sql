8e41c36a2e4cd89570730fdcc9b7a580f0e96687
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-09-01 23:12:10 -0400

    Script de loaddata arreglado, adicion de sentencia 5
    El script de load data presentaba inconsistencias en relacion al script de creacion, ya que faltaban las llaves foraneas de la tabla detalle en las tablas producto y servicio, ademas se hizo la consulta 5 de las pedidas en el enunciado

---
+++ sentencia10.sql
@@ -0,0 +1,3 @@
+-- Identificar lista de total de peluquerías por comuna, cantidad de peluquerías,
+-- cantidad de clientes residentes en la comuna.
+
---
+++ sentencia5.sql
@@ -0,0 +1,18 @@
+-- Lista de clientes que se tiñen el pelo, indicando la comuna del cliente, la peluquería
+-- donde se atendió y el valor que pagó.
+
+SELECT cli.nombre, comu.nombre, pelu.nombre,  servi.valor
+FROM cliente cli
+    INNER JOIN comuna comu
+    ON cli.id_comuna = comu.id_comuna
+        INNER JOIN  cliente_pelu cli_pelu
+        ON cli.id_cliente = cli_pelu.id_cliente
+            INNER JOIN peluqueria pelu
+            ON cli_pelu.id_peluqueria = pelu.id_peluqueria
+                INNER JOIN cita
+                ON cli_pelu.id_cliente = cita.id_cliente_pelu
+                    INNER JOIN detalle deta
+                    ON cita.id_cita = deta.id_cita
+                        INNER JOIN servicio servi
+                        ON deta.id_detalle = servi.id_detalle
+WHERE servi.nombre_servicio = 'Tinte'
