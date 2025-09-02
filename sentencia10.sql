be38cc2f855d7439a0d01502ed88fcccefbaabed
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-09-02 09:58:12 -0400

    Termino de sentencia 10, modificacion sentencia 5
    Se agregó un OR en la sentencia 5 ya que no contaba todos les servicios relacionados con el tiente y se finalizó la sentencia 10

--- sentencia10.sql
+++ sentencia10.sql
@@ -1,3 +1,17 @@
 -- Identificar lista de total de peluquerías por comuna, cantidad de peluquerías,
 -- cantidad de clientes residentes en la comuna.
 
+SELECT comu.nombre,
+       --agrupa la cantidad de peluquerias
+       COUNT(*) AS cantidad_peluquerias,
+       --agrupa los clientes de comuna
+       COUNT(*) AS cantidad_clientes,
+       --crea una lista de los nombres de las peluquerias de cada comuna
+       STRING_AGG(pelu.nombre, ', ') AS lista_peluquerias
+FROM peluqueria pelu
+         INNER JOIN comuna comu
+                    ON pelu.id_comuna = comu.id_comuna
+         INNER JOIN cliente cli
+                    ON cli.id_comuna = comu.id_comuna
+--agrupamos por la comuna
+GROUP BY comu.nombre
--- sentencia5.sql
+++ sentencia5.sql
@@ -1,6 +1,9 @@
 -- Lista de clientes que se tiñen el pelo, indicando la comuna del cliente, la peluquería
 -- donde se atendió y el valor que pagó.
 
+--haremos inner join en todas las tablas necesarias
+--solo cuando las personas cumplen con llevar el servicio
+-- Tinte y Tinte raices
 SELECT cli.nombre, comu.nombre, pelu.nombre,  servi.valor
 FROM cliente cli
     INNER JOIN comuna comu
@@ -15,4 +18,4 @@ FROM cliente cli
                     ON cita.id_cita = deta.id_cita
                         INNER JOIN servicio servi
                         ON deta.id_detalle = servi.id_detalle
-WHERE servi.nombre_servicio = 'Tinte'
+WHERE servi.nombre_servicio = 'Tinte' OR servi.nombre_servicio = 'Tinte raíces'
