15505088efeba55335691e0b3e56fdbe9251cf10
Nicolás Sarabia <nicolas.sarabia@usach.cl>
2025-09-03 22:47:36 -0400

    cambio a date_trunc

--- sentencia3.sql
+++ sentencia3.sql
@@ -3,20 +3,18 @@ WITH PAGOS_POR_MES AS (
     SELECT
         peluqueria.nombre AS nombre_peluqueria,
         empleado.id_empleado,
-        date_part('month', fecha) AS mes,
-        date_part('year', fecha) AS año,
+        date_trunc('month', fecha) AS mes_año,
         SUM(monto) AS total
     FROM SUELDO
              INNER JOIN empleado ON empleado.id_empleado = SUELDO.id_empleado
              INNER JOIN peluqueria ON peluqueria.id_peluqueria = empleado.id_peluqueria
     WHERE fecha > current_date - interval '3 years'
-    GROUP BY peluqueria.nombre, empleado.id_empleado, date_part('month', fecha), date_part('year', fecha)
+    GROUP BY peluqueria.nombre, empleado.id_empleado, mes_año
 )
-SELECT DISTINCT ON (nombre_peluqueria, mes, año)
+SELECT DISTINCT ON (nombre_peluqueria, mes_año)
     nombre_peluqueria,
     id_empleado,
-    mes,
-    año,
+    mes_año,
     total
 FROM PAGOS_POR_MES
-ORDER BY nombre_peluqueria, mes, año, total DESC;
+ORDER BY nombre_peluqueria, mes_año, total DESC;
