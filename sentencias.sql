a54d67a2cd15a6ca31caaf956c270f2e79c4238a
Patricio Bustos <patricio.bustos.n@usach.cl>
2025-09-07 22:17:41 -0300

    sentencias 5 y 10 arregladas, creacion de archivo con sentencias

--- sentencias.sql
+++ sentencias.sql
@@ -157,7 +157,7 @@ ORDER BY DATE_TRUNC('month', fecha) ASC;
 
 SELECT s.nombre_servicio as servicio_mas_caro, p.nombre as peluqueria, s.valor
 FROM servicio s
-JOIN detalle d ON s.id_detalle = d.id_detalle
+JOIN detalle d ON s.id_servicio = d.id_servicio
 JOIN cita c ON d.id_cita = c.id_cita
 JOIN cliente_pelu cp ON c.id_cliente_pelu = cp.id_cliente_pelu
 Join peluqueria p ON cp.id_peluqueria = p.id_peluqueria
@@ -165,7 +165,7 @@ WHERE s.valor = (
     -- subconsulta para obtener el valor máximo del servicio por peluquería
     SELECT MAX(s2.valor)
     FROM servicio s2
-    JOIN detalle d2 ON s2.id_detalle = d2.id_detalle
+    JOIN detalle d2 ON s2.id_servicio = d2.id_servicio
     JOIN cita c2 ON d2.id_cita = c2.id_cita
     JOIN cliente_pelu cp2 ON c2.id_cliente_pelu = cp2.id_cliente_pelu
     WHERE cp2.id_peluqueria = p.id_peluqueria
